05b41d9a9f40f17e0972d1ce2330b630e7a6ed42
esdras-santos <esantoz@protonmail.com>
2021-08-18 09:10:55 -0300

    lets test

--- contracts/PaymentProcessor.sol
+++ contracts/PaymentProcessor.sol
@@ -4,6 +4,8 @@ import './interfaces/IERC20.sol';
 import './Ownership.sol';
 
 contract PaymentProcessor is Ownership{
+
+    event Paid(address buyer, uint256 productId);
     
     uint256 private percent;
     IERC20 private token;
@@ -12,7 +14,6 @@ contract PaymentProcessor is Ownership{
     mapping (string=>address) private _tokenAddress;
     mapping (address=>mapping(string=>uint256)) private _sellerBalance;
     mapping (string=>uint256) private _ownerBalance;
-    mapping (uint256=>address) private _productSeller;
 
     modifier onlyOwner{
         require(msg.sender == owner(), "only the owner have access to that function.");
@@ -24,15 +25,23 @@ contract PaymentProcessor is Ownership{
     }
 
     //pay for the product
-    function pay(string memory _token,uint256 _amount,uint256 productId) public{
-        token = IERC20(_tokenAddress[_token]);
-        address seller = _productSeller[productId];
-        (uint256 ownerAmount, uint256 sellerAmount) = percentage(_amount, percent);
-        bool success = token.transferFrom(msg.sender,address(this), _amount);
-        if(success){
-            _sellerBalance[seller][_token] += sellerAmount;
-            _ownerBalance[_token] += ownerAmount; 
+    function pay(string memory _token,uint256 _amount,address _seller, uint256 _productPrice, uint256 _productId) public payable{
+        if (keccak256(abi.encodePacked((_token))) == keccak256(abi.encodePacked((string("ether"))))){
+            require(msg.value >= _productPrice);
+            (uint256 ownerAmount, uint256 sellerAmount) = percentage(msg.value, percent);
+            _sellerBalance[_seller][_token] += sellerAmount;
+            _ownerBalance[_token] += ownerAmount;
+        } else{
+            require(_amount >= _productPrice);
+            token = IERC20(_tokenAddress[_token]);
+            (uint256 ownerAmount, uint256 sellerAmount) = percentage(_amount, percent);
+            bool success = token.transferFrom(msg.sender,address(this), _amount);
+            if(success){
+                _sellerBalance[_seller][_token] += sellerAmount;
+                _ownerBalance[_token] += ownerAmount; 
+            }
         }
+        emit Paid(msg.sender, _productId);
     }
 
     function balanceOfSeller(address _seller,string memory _token) public view returns(uint256){
@@ -41,6 +50,12 @@ contract PaymentProcessor is Ownership{
 
     function sellerWithdraw(uint256 _amount, string memory _token) public {
         require(_sellerBalance[msg.sender][_token] >= _amount);
+        if(keccak256(abi.encodePacked((_token))) == keccak256(abi.encodePacked((string("ether"))))){
+            _sellerBalance[msg.sender][_token] -= _amount;
+            (bool success, ) = msg.sender.call.value(_amount)("");
+            require(success);
+        }
+    
         token = IERC20(_tokenAddress[_token]);
         _sellerBalance[msg.sender][_token] -= _amount;
         bool success = token.transfer(msg.sender,_amount);
@@ -49,6 +64,11 @@ contract PaymentProcessor is Ownership{
 
     function ownerWithdraw(uint256 _amount, string memory _token) public onlyOwner{
         require(_ownerBalance[_token] >= _amount);
+        if(keccak256(abi.encodePacked((_token))) == keccak256(abi.encodePacked((string("ether"))))){
+            _ownerBalance[_token] -= _amount;
+            (bool success, ) = owner().call.value(_amount)("");
+            require(success);
+        }
         token = IERC20(_tokenAddress[_token]);
         _ownerBalance[_token] -= _amount;
         bool success = token.transfer(owner(),_amount);
